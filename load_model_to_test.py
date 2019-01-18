2e0f751ab55831de17d16e0e7050475be610e389
e3ch0 <e3ch0@qq.com>
2019-01-18 11:57:47 +0800

    测试模型,并可视化结果

--- load_model_to_test.py
+++ load_model_to_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
+import sys
 
 sess = tf.InteractiveSession()
 # saver = tf.train.Saver()
@@ -21,22 +22,26 @@ images = np.ones(2 * 10 * 28 * 28 * 9 * 1)
 images = np.reshape(images, [2, 10, 28, 28, 9, 1])
 images = images.astype(np.float)
 
-# data
-test_channel_data_depth = np.load(".//data//test//test_channel_data_depth8.npy")
-test_channel_data_depth = np.reshape(test_channel_data_depth, [-1, 10, 9, 28, 28, 1])
-test_channel_data_depth = np.transpose(test_channel_data_depth, (0, 1, 3, 4, 2, 5))
-print(test_channel_data_depth.shape)
+# ".//data//test//test_channel_data_depth8.npy,"
+def test_and_save_result(file_path,save_path,test_batch=100):
+    # data
+    test_channel_data_depth = np.load(file_path)
+    test_channel_data_depth = np.reshape(test_channel_data_depth, [-1, 10, 9, 28, 28, 1])
+    test_channel_data_depth = np.transpose(test_channel_data_depth, (0, 1, 3, 4, 2, 5))
+    print(test_channel_data_depth.shape)
+    predict_result_list = []
+    for i in range(0, test_channel_data_depth.shape[0], test_batch):
+        feed_dict = {x_image: test_channel_data_depth[i:i + test_batch], keep_prob: 1.0}
+        predict_result_list.extend(sess.run(result_list, feed_dict=feed_dict))
+        print(len(predict_result_list))
+    predict_result_list = np.array(predict_result_list)
+    predict_result_list = np.reshape(predict_result_list, [122, 122])
+    np.save(save_path, predict_result_list)
 
-test_batch = 100
-predict_result_list = []
-for i in range(0, test_channel_data_depth.shape[0], test_batch):
-    feed_dict = {x_image: test_channel_data_depth[i:i + test_batch], keep_prob: 1.0}
-    predict_result_list.extend(sess.run(result_list, feed_dict=feed_dict))
-    print(len(predict_result_list))
-
-predict_result_list = np.array(predict_result_list)
-predict_result_list=np.reshape(predict_result_list, [122,122])
-
-np.save(".//visual//data",predict_result_list)
-plt.imshow(predict_result_list, cmap='gray')
-plt.show()
+for i in range(0,19):
+    file_parent_path=".//data//test//"
+    if len( sys.argv)>1:
+        file_parent_path=sys.argv[1]
+    file_path=file_parent_path+"test_channel_data_depth"+str(i)+".npy"
+    save_path=".//visual//data//test_channel_data_depth"+str(i)
+    test_and_save_result(file_path,save_path)
